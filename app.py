86027f71f1f807f54041b68a1567bad76219d9d5
Mohamed B. <mohamed.benighil07@gmail.com>
2025-07-13 13:18:01 +0200

    deploy

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
 
 # create_app() to use factory pattern: avoid multiple db connections, tests etc...
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://****:****@****.eoruayv.mongodb.net/")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.blog
 
     # si on veut ajouter des données de test (sans utiliser la base de données  )
@@ -21,7 +24,7 @@ def create_app():
                 date = datetime.datetime.now().strftime("%Y-%m-%d")
                 # si données de test
                 # entries.append((entry, date, date_format))
-                app.db.collection.insert_one({"entry": entry, "date": date})
+                app.db.entries.insert_one({"entry": entry, "date": date})
                 return redirect(url_for("home"))
         entries = [( entry["entry"], entry["date"], datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")) for entry in app.db.entries.find() if entry.get("entry")]
         return render_template("home.html", entries=entries)
